934fbac3eaf0831c07b0db403efed12a8466bdb2
Marvine Li <lidaqian11@gmail.com>
2023-08-06 22:09:38 +0800

    fix: [calculator] use object state

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -28,6 +28,12 @@ function InputButton(props: { token?: Token; displayValue?: string; backgroundGr
   )
 }
 
+interface CalculatorState {
+  tokens: Token[]
+  result: string
+  needReset: boolean
+}
+
 function Calculator() {
   const LEFT_PARENTHESES = { type: TokenType.LEFT_PARENTHESES, value: '(' }
   const RIGHT_PARENTHESES = { type: TokenType.RIGHT_PARENTHESES, value: ')' }
@@ -49,80 +55,90 @@ function Calculator() {
   const NUMBER_9 = { type: TokenType.NUMBER, value: '9' }
   const DOT = { type: TokenType.NUMBER, value: '.' }
 
-  const [tokens, setTokens] = useState<Token[]>([])
-  const [result, setResult] = useState<string>()
-
-  const lastToken = () => tokens[tokens.length - 1]
-  const firstToken = () => tokens[0]
-  const clearTokens = () => {
-    setTokens([])
-    setResult('')
+  const [state, setState] = useState<CalculatorState>({
+    tokens: [],
+    result: '',
+    needReset: false,
+  })
+  const lastToken = state.tokens[state.tokens.length - 1]
+  const firstToken = state.tokens[0]
+  function addOperator(token: Token) {
+    if (resetTokensIfNeeded(token))
+      return
+    setState({ ...state, tokens: [...state.tokens, token] })
   }
-  const removeLastToken = () => {
-    setTokens(tokens.slice(0, -1))
-  }
-  const removeFirstToken = () => {
-    setTokens(tokens.slice(1))
-  }
-  const addFirstToken = (token: Token) => {
-    setTokens([token, ...tokens])
-  }
-  const addLastToken = (token: Token) => {
-    setTokens([...tokens, token])
-  }
-  const addNumber = (token: Token) => {
-    if (tokens.length === 0 || lastToken().type !== TokenType.NUMBER)
-      addLastToken(token)
+  function addNumber(token: Token) {
+    if (resetTokensIfNeeded(token))
+      return
+    if (state.tokens.length === 0 || lastToken.type !== TokenType.NUMBER)
+      setState({ ...state, tokens: [...state.tokens, token] })
     else
-      tokens[tokens.length - 1] = { ...lastToken(), value: lastToken().value + token.value }
+      setState({ ...state, tokens: [...state.tokens.slice(0, -1), { ...lastToken, value: lastToken.value + token.value }] })
   }
-  const changeNegitive = () => {
-    if (tokens.length === 0)
-      addLastToken(MINUS)
+  function makeNegative() {
+    if (resetTokensIfNeeded(MINUS))
+      return
+    if (state.tokens.length === 0)
+      setState({ ...state, tokens: [MINUS] })
+    else if (firstToken.type !== TokenType.MINUS)
+      setState({ ...state, tokens: [MINUS, ...state.tokens] })
     else
-      firstToken().type === TokenType.MINUS ? removeFirstToken() : addFirstToken(MINUS)
+      setState({ ...state, tokens: state.tokens.slice(1) })
   }
-  const calculate = () => {
-    setResult(new Parser(tokens).parse().evaluate().toString())
+  function evaluate() {
+    setState({ ...state, result: new Parser(state.tokens).parse().evaluate().toString(), needReset: true })
+  }
+  function resetTokensIfNeeded(firstToken: Token) {
+    if (state.needReset) {
+      setState({ ...state, tokens: [firstToken], result: '', needReset: false })
+      return true
+    }
+    return false
+  }
+  function clearTokens() {
+    setState({ ...state, tokens: [], result: '', needReset: false })
+  }
+  function removeLastToken() {
+    setState({ ...state, tokens: state.tokens.slice(0, -1) })
   }
 
   return (
     <div className="grid grid-cols-[repeat(4,5rem)] grid-rows-[minmax(8rem,auto)_repeat(6,4rem)] justify-center mt-8 gap-px">
 
       <div className="col-span-4 bg-black/[.75] flex flex-col items-end justify-around p-3 break-words rounded-md">
-        <div className="text-2xl text-white">{tokens.map(v => v.value).join('')}</div>
-        <div className="text-4xl text-white">{result}</div>
+        <div className="text-2xl text-white">{state.tokens.map(v => v.value).join('')}</div>
+        <div className="text-4xl text-white">{state.result}</div>
       </div>
 
-      <InputButton token={LEFT_PARENTHESES} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(LEFT_PARENTHESES)}/>
-      <InputButton token={RIGHT_PARENTHESES} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(RIGHT_PARENTHESES)}/>
+      <InputButton token={LEFT_PARENTHESES} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(LEFT_PARENTHESES)}/>
+      <InputButton token={RIGHT_PARENTHESES} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(RIGHT_PARENTHESES)}/>
       <InputButton displayValue="C" backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => clearTokens()}/>
       <InputButton displayValue="DEL" backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => removeLastToken()}/>
 
-      <InputButton token={PERCENT} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(PERCENT)}/>
-      <InputButton token={POWER} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(POWER)}/>
+      <InputButton token={PERCENT} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(PERCENT)}/>
+      <InputButton token={POWER} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(POWER)}/>
       <InputButton displayValue="C" backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => clearTokens()}/>
-      <InputButton token={DIVIDE} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(DIVIDE)}/>
+      <InputButton token={DIVIDE} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(DIVIDE)}/>
 
       <InputButton token={NUMBER_7} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_7)}/>
       <InputButton token={NUMBER_8} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_8)}/>
       <InputButton token={NUMBER_9} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_9)}/>
-      <InputButton token={MULTIPLY} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(MULTIPLY)}/>
+      <InputButton token={MULTIPLY} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(MULTIPLY)}/>
 
       <InputButton token={NUMBER_4} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_4)}/>
       <InputButton token={NUMBER_5} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_5)}/>
       <InputButton token={NUMBER_6} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_6)}/>
-      <InputButton token={MINUS} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(MINUS)}/>
+      <InputButton token={MINUS} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(MINUS)}/>
 
       <InputButton token={NUMBER_1} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_1)}/>
       <InputButton token={NUMBER_2} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_2)}/>
       <InputButton token={NUMBER_3} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_3)}/>
-      <InputButton token={PLUS} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addLastToken(PLUS)}/>
+      <InputButton token={PLUS} backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => addOperator(PLUS)}/>
 
-      <InputButton displayValue="+/-" backgroundGroup={BackgroundGroup.NUMBER} onClick={() => changeNegitive()}/>
+      <InputButton displayValue="+/-" backgroundGroup={BackgroundGroup.NUMBER} onClick={() => makeNegative()}/>
       <InputButton token={NUMBER_0} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(NUMBER_0)}/>
       <InputButton token={DOT} backgroundGroup={BackgroundGroup.NUMBER} onClick={() => addNumber(DOT)}/>
-      <InputButton displayValue="=" backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => calculate()}/>
+      <InputButton displayValue="=" backgroundGroup={BackgroundGroup.OPERATOR} onClick={() => evaluate()}/>
     </div>
   )
 }
